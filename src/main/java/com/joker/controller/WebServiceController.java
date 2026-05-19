0e88f43edf297a3dc287a77f9f54634482e057c8
Waidiney <dineyjr@hotmail.com>
2026-05-19 13:11:26 -0300

    teste joker

---
+++ src/main/java/com/joker/controller/WebServiceController.java
@@ -0,0 +1,12 @@
+package com.joker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebServiceController {
+    @GetMapping(value = "/joker")
+    public String joker(){
+        return "Fica aí com tuas piadinhas";
+    }
+}
